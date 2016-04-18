567f511201436be611b73b5cc0181b1d0ed551c3
fangyuxi <xcoder.fang@gmail.com>
2016-04-18 20:40:11 +0800

    init

---
+++ Example/Pods/Target Support Files/HYAlbum/HYAlbum-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HYAlbum : NSObject
+@end
+@implementation PodsDummy_HYAlbum
+@end
---
+++ Example/Pods/Target Support Files/HYAlbum/HYAlbum-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double HYAlbumVersionNumber;
+FOUNDATION_EXPORT const unsigned char HYAlbumVersionString[];
+
